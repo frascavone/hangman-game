ce9ccb3b4358753492109cf8041083035c934589
frascavone <99489016+frascavone@users.noreply.github.com>
2023-02-22 18:13:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// Hangman Game
-
-// Hangman is a simple game that revolves around guessing
-// a randomly chosen word.
-// The player gets to choose an alphabet every round.
-// If the alphabet does not exist in the chosen word,
-// the player will receive one strike.
-// After receiving a certain number of strikes,
-// the player loses the game.
-// The player will win the game if he/she/it
-// correctly guesses all the alphabets.
-
-// Come requisiti aggiuntivi ti aggiungo:
-
-// - fai in modo che la tastiera sia a video
-//   e non che gli input arrivino da tastiera hardware
-// - la tastiera può avere un diverso layout (qwerty, abcde, ecc)
-// - usa servizi API come swapi.dev per recuperare le parole da indovinare
-
-//////////////////////////////////////////////////////////
 'use scrict';
 
 import * as canvas from './canvas.js';
